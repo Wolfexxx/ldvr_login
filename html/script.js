f06c06b87159885b7c9431b4a89f9c49b988e017
wolfex <91018450+wolfex01@users.noreply.github.com>
2022-01-30 20:19:21 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,3 +1,5 @@
+/// Elementek
+
 const login = $('.login')
 const register = $('.register')
 const username = $('.username')
@@ -6,19 +8,21 @@ const pass = $('.password')
 window.addEventListener('message', function(event) {
     var data = event.data
     if (data.type === 'open') {
-        console.log('open')
+        console.log('LDVR Loginsystem | OPEN')
         $('body').css('display', 'grid');
     } if (data.type === 'close') {
-        console.log('close')
+        console.log('LDVR Loginsystem | CLOSE')
         $('body').css('display', 'none');
     }
 })
 
+/// Login Clickelés
 
 login.on('click', () => {
     $.post('https://ldvr_login/login', JSON.stringify({name: username.val(), password: pass.val()}));
 })
 
+/// Register Clickelés
 
 register.on('click', () =>{ 
     $.post('https://ldvr_login/register', JSON.stringify({name: username.val(), password: pass.val()}));
